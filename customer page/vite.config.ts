782f83e6905ba1c09b404c20ff395e0fd5e08d5c
Birat Sharma <birat.sharma@khalti.com>
2026-06-25 16:52:43 +0545

    Fixed vite script build

--- customer page/vite.config.ts
+++ customer page/vite.config.ts
@@ -5,29 +5,30 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
-  },
-  vite: {
-    server: {
-      hmr: {
-        host: "careers.khalti.com",
-        protocol: "wss",
-        clientPort: 443,
+  plugins: [
+    tanstackStart({ server: { entry: "server" } }),
+    react(),
+    tailwindcss(),
+    tsConfigPaths(),
+  ],
+  server: {
+    hmr: {
+      host: "careers.khalti.com",
+      protocol: "wss",
+      clientPort: 443,
+    },
+    allowedHosts: ["careers.khalti.com", "recruiters.khalti.com"],
+    watch: {
+      ignored: ["**/*"],
+    },
+    proxy: {
+      "/api": {
+        target: process.env.API_INTERNAL_URL,
+        changeOrigin: true,
       },
-      allowedHosts: ["careers.khalti.com", "recruiters.khalti.com"],
-      watch: {
-        ignored: ["**/*"],
-      },
-      proxy: {
-        "/api": {
-          target: process.env.API_INTERNAL_URL,
-          changeOrigin: true,
-        },
-        "/static": {
-          target: process.env.API_INTERNAL_URL,
-          changeOrigin: true,
-        },
+      "/static": {
+        target: process.env.API_INTERNAL_URL,
+        changeOrigin: true,
       },
     },
   },
